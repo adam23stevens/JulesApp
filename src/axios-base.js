a26baf456a5493b62384956732953a11a3f0ac1e
Adam <adam23stevens@gmail.com>
2018-02-17 18:46:57 +0000

    clues from db

--- src/axios-base.js
+++ src/axios-base.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: ''
+    baseURL: 'https://jules-app.firebaseio.com/'
 });
 
 export default instance;
--- src/containers/Clues/clues.js
+++ src/containers/Clues/clues.js
@@ -3,21 +3,47 @@ import classes from './clues.css';
 import data from './data';
 import Clue from './Clue/Clue';
 import NavigationItem from '../../components/Navigation/NavigationItems/NavigationItem/NavigationItem';
+import axios from '../../axios-base';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 
 class Clues extends Component {
 
+    state = {
+        cluesState: null,
+        error: false
+    }
+
+    componentDidMount() {
+        axios.get('https://jules-app.firebaseio.com/clues.json')
+            .then(response => {
+                this.setState({cluesState: response.data});
+            })
+            .catch(error => {
+                this.setState({error: true})
+            })
+    }
+
     render() {
-        const allClues = data.filter(d => d.clue.isShown).map((d, index) => {
+        let allClues = this.state.error ? <p>clues can''t be loaded</p> : <Spinner/>
+        if (this.state.cluesState) {
+        allClues = (Object.keys(this.state.cluesState).map(clueKey => {
+            return this.state.cluesState[clueKey]
+        }));
+        console.log(allClues);
+
+        allClues = allClues.filter(d => d.isShown).map((d, index) => {
             index += 1;
-            const linkTo = '/clue/' + d.clue.id;
-            const clueName = 'Clue: ' + index;
+            const linkTo = '/clue/' + d.id;
+            const clueName = d.id;
             return (
-            <li className='clueItem'>
+            <li key={index} className='clueItem'>
                 <NavigationItem link={linkTo}>{clueName}</NavigationItem>
             </li>
             )
         })
+    }
 
         return (
             <div className='clues'>
@@ -30,4 +56,4 @@ class Clues extends Component {
     }
 }
 
-export default Clues;
+export default withErrorHandler(Clues, axios);
